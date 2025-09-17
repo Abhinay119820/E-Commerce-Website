08d659f76c213549d4582426d18dbc025fcb1162
Abhinay Dasari <abhinaydasari1520.com>
2025-09-18 03:33:37 +0530

    Third commit added mp4 video and completed the home page

--- app.js
+++ app.js
@@ -1,25 +1,3 @@
-// let currentIndex = 0
-// function showSlide(index){
-// const slides = document.querySelector("slide-bar")
-// const TotalSlides = document.querySelectorAll('.slide-bar img').length
-
-// if(index >= TotalSlides){
-//     currentIndex =0
-// }else if(index < 0){
-//     currentIndex = TotalSlides-1
-// }else {
-//     currentIndex = index;
-// }
-
-//   slides.style.transform = `translateX(-${currentIndex * 100}%)`;
-// }
-// function nextSlide(){
-//     showSlide(currentIndex + 1)
-// }
-// function prevSlide(){
-//     showSlide(currentIndex - 1)
-// }
-// setInterval(nextSlide,1500)
 
  let index = 0;
     const slides = document.getElementById("slide-container");
