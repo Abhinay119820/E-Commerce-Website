2f5062e9a8b449b16c3132942a79e6e8eb0e5e82
Abhinay Dasari <abhinaydasari1520.com>
2025-09-18 06:45:07 +0530

    fourth commit footer completed and slidebar images removed from the code and added the cards in the homepage

--- app.js
+++ app.js
@@ -1,21 +1,21 @@
 
- let index = 0;
-    const slides = document.getElementById("slide-container");
-    const totalSlides = slides.children.length;
+//  let index = 0;
+//     const slides = document.getElementById("slide-container");
+//     const totalSlides = slides.children.length;
 
-    function showSlide() {
-      slides.style.transform = `translateX(${-index * 600}px)`;
-    }
+//     function showSlide() {
+//       slides.style.transform = `translateX(${-index * 600}px)`;
+//     }
 
-    function nextSlide() {
-      index = (index + 1) % totalSlides;
-      showSlide();
-    }
+//     function nextSlide() {
+//       index = (index + 1) % totalSlides;
+//       showSlide();
+//     }
 
-    function prevSlide() {
-      index = (index - 1 + totalSlides) % totalSlides;
-      showSlide();
-    }
+//     function prevSlide() {
+//       index = (index - 1 + totalSlides) % totalSlides;
+//       showSlide();
+//     }
 
-    // Auto-slide every 3 seconds
-    setInterval(nextSlide, 2000);
+//     // Auto-slide every 3 seconds
+//     setInterval(nextSlide, 2000);
